8de7f6db252c868677ce19b0e37da848cd448386
Faisal Manzer <faisal_manzer@yahoo.in>
2020-06-06 05:29:48 +0530

    Configured ONESIGNAL push notification

--- native/App.js
+++ native/App.js
@@ -1,44 +1,175 @@
-import React, { useEffect } from 'react';
-import { StatusBar } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
 
-import SplashScreen from 'react-native-splash-screen';
-import 'react-native-gesture-handler';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Provider, useDispatch } from 'react-redux';
+import React, { Component } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
 
-import { store } from 'common/reducers';
-import { sessionStarted } from 'common/actions/demo';
-
-import { GettingStarted } from './src/components/GettingStarted';
-import { MonorepoIntro } from './src/components/MonorepoIntro';
-
-const Stack = createStackNavigator();
-
-const Initial = () => {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    SplashScreen.hide();
-    dispatch(sessionStarted());
-  }, []);
-
-  return null;
-};
+import OneSignal from 'react-native-onesignal'; // Import package from node modules
+import secrets from 'common/secrets';
 
 
-const App = () => (
-  <Provider store={store}>
-    <StatusBar barStyle='dark-content' />
-    <Initial />
-    <NavigationContainer screenOptions={{ headerShown: false }}>
-      <Stack.Navigator initialRouteName='Home'>
-        <Stack.Screen name='Home' component={GettingStarted} />
-        <Stack.Screen name='Intro' component={MonorepoIntro} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  </Provider>
-);
+function myiOSPromptCallback(permission) {
+  // do something with permission value
+}
 
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+class App extends Component {
+
+  constructor(properties) {
+    super(properties);
+    
+    // Remove this method to stop OneSignal Debugging
+    OneSignal.setLogLevel(6, 0);
+
+    // Replace 'YOUR_ONESIGNAL_APP_ID' with your OneSignal App ID.
+    OneSignal.init(secrets.ONESIGNAL_APP_ID, {
+      kOSSettingsKeyAutoPrompt: false,
+      kOSSettingsKeyInAppLaunchURL: false,
+      kOSSettingsKeyInFocusDisplayOption: 2,
+    });
+    OneSignal.inFocusDisplaying(2);
+    // Controls what should happen if a notification is received while the app is open.
+    // 2 means that the notification will go directly to the device's notification center.
+
+    // The promptForPushNotifications function code will show the iOS push notification prompt.
+    // We recommend removing the following code and instead using an
+    // In-App Message to prompt for notification permission (See step below)
+    OneSignal.promptForPushNotificationsWithUserResponse(myiOSPromptCallback);
+
+    OneSignal.addEventListener('received', this.onReceived);
+    OneSignal.addEventListener('opened', this.onOpened);
+    OneSignal.addEventListener('ids', this.onIds);
+  }
+
+  componentWillUnmount() {
+    OneSignal.removeEventListener('received', this.onReceived);
+    OneSignal.removeEventListener('opened', this.onOpened);
+    OneSignal.removeEventListener('ids', this.onIds);
+  }
+
+  onReceived(notification) {
+    console.log('Notification received: ', notification);
+  }
+
+  onOpened(openResult) {
+    console.log('Message: ', openResult.notification.payload.body);
+    console.log('Data: ', openResult.notification.payload.additionalData);
+    console.log('isActive: ', openResult.notification.isAppInFocus);
+    console.log('openResult: ', openResult);
+  }
+
+  onIds(device) {
+    console.log('Device info: ', device);
+  }
+
+  render() {
+    return (
+      <>
+        <StatusBar barStyle='dark-content' />
+        <SafeAreaView>
+          <ScrollView
+            contentInsetAdjustmentBehavior='automatic'
+            style={styles.scrollView}>
+            <Header />
+            {global.HermesInternal == null ? null : (
+              <View style={styles.engine}>
+                <Text style={styles.footer}>Engine: Hermes</Text>
+              </View>
+            )}
+            <View style={styles.body}>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Step One</Text>
+                <Text style={styles.sectionDescription}>
+                  Edit 
+                  {' '}
+                  <Text style={styles.highlight}>App.js</Text>
+                  {' '}
+                  to change this
+                  screen and then come back to see your edits.
+                </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>See Your Changes</Text>
+                <Text style={styles.sectionDescription}>
+                  <ReloadInstructions />
+                </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Debug</Text>
+                <Text style={styles.sectionDescription}>
+                  <DebugInstructions />
+                </Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Learn More</Text>
+                <Text style={styles.sectionDescription}>
+                  Read the docs to discover what to do next:
+                </Text>
+              </View>
+              <LearnMoreLinks />
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </>
+    );
+  }
+}
 
 export default App;
