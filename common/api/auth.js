0c85993c0200d23d7464865b583153e97942b45e
Faisal Manzer <faisal_manzer@yahoo.in>
2020-06-21 16:56:31 +0530

    ..

--- common/api/auth.js
+++ common/api/auth.js
@@ -1,7 +1,7 @@
 import api from 'common/api/index';
 
-export const signInAPI = ({ googleId, token }) =>
-  api.post(`/auth/sign-in/google/`, { googleId, token }, { secure: false });
+export const signInAPI = ({googleId, token}) =>
+  api.post(`/auth/sign-in/google/`, {googleId, token}, {secure: false});
 
 export const getUserMetaAPI = () => api.get(`/auth/meta/`);
 
--- native/App.js
+++ native/App.js
@@ -2,27 +2,27 @@ import React from 'react';
 import 'react-native-gesture-handler';
 
 import codePush from 'react-native-code-push';
-import { StatusBar } from 'react-native';
+import {StatusBar} from 'react-native';
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
 import AsyncStorage from '@react-native-community/async-storage';
-import { Provider } from 'react-redux';
-import { encode as btoa } from 'base-64';
+import {Provider} from 'react-redux';
+import {encode as btoa} from 'base-64';
 
-import { getStore } from 'common/reducers';
-import { getStorage } from 'common/storage';
+import {getStore} from 'common/reducers';
+import {getStorage} from 'common/storage';
 
-import { notify } from 'helpers/notify';
-import { css } from 'styles';
+import {notify} from 'helpers/notify';
+import {css} from 'styles';
 
-import { Initial } from 'components/Initial';
-import { GettingStarted } from 'components/GettingStarted';
-import { MonorepoIntro } from 'components/MonorepoIntro';
-import { useNotification } from 'hooks/notification';
+import {Initial} from 'components/Initial';
+import {GettingStarted} from 'components/GettingStarted';
+import {MonorepoIntro} from 'components/MonorepoIntro';
+import {useNotification} from 'hooks/notification';
 
 const Stack = createStackNavigator();
-const { store, persistor } = getStore(AsyncStorage);
+const {store, persistor} = getStore(AsyncStorage);
 
 global.persistor = persistor;
 global.storage = getStorage(AsyncStorage);
@@ -35,11 +35,11 @@ const App = () => {
 
   return (
     <Provider store={store}>
-      <StatusBar barStyle='dark-content' />
-      <NavigationContainer screenOptions={{ headerShown: false }}>
-        <Stack.Navigator initialRouteName='Home'>
-          <Stack.Screen name='Home' component={GettingStarted} />
-          <Stack.Screen name='Intro' component={MonorepoIntro} />
+      <StatusBar barStyle="dark-content" />
+      <NavigationContainer screenOptions={{headerShown: false}}>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={GettingStarted} />
+          <Stack.Screen name="Intro" component={MonorepoIntro} />
         </Stack.Navigator>
       </NavigationContainer>
       <Initial />
